bb57d23f93577f601082e3d9dea862870747470f
Kyle N. Crabtree <kncrabtree@ucdavis.edu>
2017-04-08 15:39:21 -0700

    Unlock PC9518 keypad on connect

--- qc9518.cpp
+++ qc9518.cpp
@@ -60,6 +60,7 @@ bool QC9518::testConnection()
     blockSignals(false);
 
     pGenWriteCmd(QString(":SPULSE:STATE 1\n"));
+    pGenWriteCmd(QString(":SYSTEM:KLOCK 0\n"));
 
     emit configUpdate(d_config);
     emit connected();
